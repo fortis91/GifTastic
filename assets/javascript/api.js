0a01ec661f9c4b4850132f1b767fd6d9a7cef976
Nigel Phillips <neastern101@yahoo.com>
2019-04-18 09:18:25 -0500

    updated front-end

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,12 +1,13 @@
 const apiKey = "lIs3ok8bg7ciOQExZvbuD2howm6mCwTe";
 const rating = "G";
 const lang = "eng";
-const limit = 2;
+const limit = 10;
 var gifObjs = [];
 var gifKey = 0;
 
 
 $(document).on("click", ".gif", function () {
+    console.log("click");
     var id = $(this).attr("id");
     var state = $(this).attr("gif-state");
     console.log("click on gif: " + id);
@@ -33,14 +34,17 @@ function retrieveData() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        console.log(response);
         results = response.data;
         //formatJSON(results);
 
-        for (var j = 0; j < results.length; j++) {
+        for (var i = 0; i < results.length; i++) {
             gifObjs.push({
                 key: gifKey,
-                still: results[j].images.fixed_height_still.url,
-                animate: results[j].images.fixed_height.url
+                rating: results[i].rating,
+                title: results[i].title,
+                still: results[i].images.fixed_height_still.url,
+                animate: results[i].images.fixed_height.url
             })
             gifKey++;
         }
@@ -48,27 +52,66 @@ function retrieveData() {
 
         $("#gifs-view").empty();
 
-        for (var i = 0; i < gifObjs.length; i++) {
-            console.log(gifObjs[i].key);
-            var gifDiv = $('<div>');
-            gifDiv.attr("class", "col-lg-3 col-md-4 col-6");
+        // <div class="mdb-lightbox">
+        //     <figure class="col-md-4">
+        //  </figure >
+        //         <img src="https://media2.giphy.com/media/JIX9t2j0ZTN9S/200_s.gif" gif-state="still"
+        //         class="img-fluid" id="3">
+        // </div>
 
-            var a = $('<a>');
-            a.attr("href", "#");
-            // a.attr("class", "d-block mb-4 h-100");
+        // for (var i = 0; i < gifObjs.length; i++) {
+        //     var gifDiv = $("<div>");
+        //     gifDiv.addClass("mdb-lighbox");
+        //     var figure = $("<figure>");
+        //     figure.addClass("col-md-4");
+        //     var image = $("<img>");
+        //     image.attr("source", "https://media2.giphy.com/media/JIX9t2j0ZTN9S/200_s.gif");
+        //     gifDiv.append(figure, image);
+        //     $("#gifs-view").prepend(gifDiv);
+        // }
+        // <div class="mdb-lightbox">
+        //     <figure class="col-md-4">
+        //         <img alt="picture"
+        //             src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(145).jpg"
+        //             class="img-fluid">
+        //      </figure>
+        // </div>
+        for (var i = 0; i < gifObjs.length; i++) {
+            var gifDiv = $('<div>');
+            gifDiv.addClass("mdb-lightbox");
+            var figure = $('<figure>');
+            figure.addClass('col-md-4');
+        
+                    //     var p = $('<p>').html("Rating: " + gifObjs[i].rating.toUpperCase());
 
             var image = $('<img>');
             image.attr("src", gifObjs[i].still);
             image.attr("gif-state", "still");
-            image.attr("alt", "");
-            image.attr("class", "gif img-fluid img-thumbnail");
-            // image.attr("id", i);
+            image.attr("class", "gif img-fluid");
             image.attr("id", gifObjs[i].key);
-
-            gifDiv.append(a, image);
+            figure.append(image);
+            gifDiv.append(figure);
             $("#gifs-view").prepend(gifDiv);
-            // $("#gifs-view").append(gifDiv);
         }
+        // for (var i = 0; i < gifObjs.length; i++) {
+        //     console.log(gifObjs[i].key);
+        //     var gifDiv = $('<div>');
+        //     gifDiv.attr("class", "col-lg-3 col-md-4 col-6");
+
+        //     var a = $('<a>');
+        //     a.attr("href", "#");
+        //     // a.attr("class", "d-block mb-4 h-100");
+        //     var p = $('<p>').html("Rating: " + gifObjs[i].rating.toUpperCase());
+        //     var image = $('<img>');
+        //     image.attr("src", gifObjs[i].still);
+        //     image.attr("gif-state", "still");
+        //     image.attr("alt", "");
+        //     image.attr("class", "gif img-fluid img-thumbnail");
+        //     image.attr("id", gifObjs[i].key);
+
+        //     gifDiv.append(p, a, image);
+        //     $("#gifs-view").prepend(gifDiv);
+        // }
         console.log(gifObjs)
         return results;
     });
